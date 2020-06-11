ec5e90e6072acca95db1e949ec5daff2c9bdcbb6
Jeremy Marshall <jeremystuartmarshall@gmail.com>
2020-06-12 08:59:52 +1000

    codecov script

---
+++ go.test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out \
+        -covermode=atomic \
+        -coverpkg github.com/JeremyMarshall/gqlgen-jwtapi \
+        "$d"
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
